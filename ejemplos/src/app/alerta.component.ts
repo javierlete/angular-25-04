231491ad87c45166bb1bf93e05f2f7e8a3edd4fc
javier <javier@ipartekaula.com>
2024-04-24 17:18:44 +0200

    Estilo para la alerta

--- ejemplos/src/app/alerta.component.ts
+++ ejemplos/src/app/alerta.component.ts
@@ -3,6 +3,16 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'alerta',
     template: '<div>Alerta</div>',
+    styles: `div { 
+        text-align: center; 
+        background-color: red;
+        border: 1px solid lightgray;
+        border-radius: 0.25rem;
+        padding: 0.25rem;
+        color: white;
+        font-weight: bold;
+        font-family: sans-serif;
+    }`,
     standalone: true,
 })
 export class AlertaComponent {}
