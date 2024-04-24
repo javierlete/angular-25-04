ac01ec25db9dc0e57a544d3b9c7a1b282e2a1612
javier <javier@ipartekaula.com>
2024-04-24 17:43:33 +0200

    Atributo color

--- ejemplos/src/app/alerta.component.ts
+++ ejemplos/src/app/alerta.component.ts
@@ -2,19 +2,22 @@ import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'alerta',
-    template: '<div>{{mensaje}}</div>',
+    template: '<div style="background-color: {{color}}">{{mensaje}}</div>',
     styles: `div { 
         text-align: center; 
-        background-color: red;
+        -webkit-text-stroke-width: 1px;
+        -webkit-text-stroke-color: black;
         border: 1px solid lightgray;
         border-radius: 0.25rem;
         padding: 0.25rem;
         color: white;
         font-weight: bold;
         font-family: sans-serif;
+        font-size: 150%;
     }`,
     standalone: true,
 })
 export class AlertaComponent {
     @Input() mensaje = 'Alerta de prueba';
+    @Input() color = 'red';
 }
