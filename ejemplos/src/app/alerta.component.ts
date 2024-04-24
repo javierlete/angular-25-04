4bce6387da2e0826eb5e642ebbfeb9006aeb65de
javier <javier@ipartekaula.com>
2024-04-24 17:12:57 +0200

    Creación de AlertaComponent

---
+++ ejemplos/src/app/alerta.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'alerta',
+    template: '<div>Alerta</div>',
+    standalone: true,
+})
+export class AlertaComponent {}
--- ejemplos/src/app/app.component.ts
+++ ejemplos/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AlertaComponent } from './alerta.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AlertaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
