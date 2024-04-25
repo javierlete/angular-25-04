9a5327168f9a01330f4e63ebdd855340397d04f8
javier <javier@ipartekaula.com>
2024-04-25 18:00:05 +0200

    ng g c persona --inlin e-template --skip-tests

---
+++ ejemplos/src/app/persona/persona.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      persona works!
+    </p>
+  `,
+  styleUrl: './persona.component.css'
+})
+export class PersonaComponent {
+
+}
