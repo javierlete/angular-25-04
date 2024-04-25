a0034cf3d1c3d28c0c4924f1a0872e37c5625e5b
javier <javier@ipartekaula.com>
2024-04-25 18:14:21 +0200

    Lo colocamos en pantalla

--- ejemplos/src/app/principal/principal.component.ts
+++ ejemplos/src/app/principal/principal.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LabelInputComponent } from '../label-input/label-input.component';
+import { PersonaComponent } from '../persona/persona.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [LabelInputComponent],
+  imports: [LabelInputComponent, PersonaComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
