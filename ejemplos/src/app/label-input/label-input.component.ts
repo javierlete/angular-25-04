2385ed6c9ee4417cb356082338617c69d426e317
javier <javier@ipartekaula.com>
2024-04-25 17:24:08 +0200

    LabelInput con id, etiqueta y tipo

--- ejemplos/src/app/label-input/label-input.component.ts
+++ ejemplos/src/app/label-input/label-input.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-label-input',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      label-input works!
-    </p>
+    <div>
+        <label for="{{id}}">{{etiqueta}}</label>
+        <input type="{{tipo}}" id="{{id}}">
+    </div>
   `,
-  styles: ``
+  styles: `
+    div {
+      display: flex;
+      align-items: baseline;
+    }
+
+    label {
+      width: 10rem;
+    }
+
+    input {
+      flex-grow: 1;
+      margin: 0.5rem 0;
+
+      padding: 0.5rem;
+      border: 1px solid darkgray;
+      border-radius: 0.5rem;
+    }
+  `
 })
 export class LabelInputComponent {
-
+  @Input() etiqueta: string = '';
+  @Input() id: string = '';
+  @Input() tipo: string = 'text';
 }
