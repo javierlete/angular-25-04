ffb7c7286b3f6ba01214054d4ee1540f633a86c8
javier <javier@ipartekaula.com>
2024-04-25 18:25:47 +0200

    Creamos una persona concreta en principal

--- ejemplos/src/app/principal/principal.component.ts
+++ ejemplos/src/app/principal/principal.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LabelInputComponent } from '../label-input/label-input.component';
 import { PersonaComponent } from '../persona/persona.component';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-principal',
@@ -11,10 +12,16 @@ import { PersonaComponent } from '../persona/persona.component';
 })
 export class PrincipalComponent {
   nombre = '';
+  pepe: Persona = { 
+    id: 2, 
+    nombre: 'Pepe', 
+    apellidos: 'Pérez', 
+    fechaNacimiento: new Date('2000-01-02') 
+  };
   saludar(nombreRecibido: string) {
     this.nombre = nombreRecibido;
   }
-  
+
   onNombreCambiado(nombreRecibido: string) {
     this.nombre = nombreRecibido;
   }
