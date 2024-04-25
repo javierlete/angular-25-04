086e62657f4b38fc968e9a68318c7af561e588fc
javier <javier@ipartekaula.com>
2024-04-25 16:28:13 +0200

    Borramos el componente viejo

--- ejemplos/src/app/principal.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    standalone: true,
-    selector: 'app-principal',
-    template: '<main>Principal</main>'
-})
-export class PrincipalComponent {}
