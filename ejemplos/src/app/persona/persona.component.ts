91baa5b4334e6ba6f48cece9acc940d076449274
javier <javier@ipartekaula.com>
2024-04-25 18:13:59 +0200

    Modificamos PersonaComponent para que use Persona

--- ejemplos/src/app/persona/persona.component.ts
+++ ejemplos/src/app/persona/persona.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-persona',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      persona works!
-    </p>
+    <dl>
+      <dt>Id</dt>
+      <dd>{{persona.id}}</dd>
+      <dt>Nombre</dt>
+      <dd>{{persona.nombre}}</dd>
+      <dt>Apellidos</dt>
+      <dd>{{persona.apellidos}}</dd>
+      <dt>Fecha de nacimiento</dt>
+      <dd>{{persona.fechaNacimiento?.toLocaleDateString()}}</dd>
+    </dl>
   `,
   styleUrl: './persona.component.css'
 })
 export class PersonaComponent {
-
+  persona: Persona = {
+    id: 1,
+    nombre: 'Javier',
+    apellidos: 'Lete',
+    fechaNacimiento: new Date('2000-01-02'),
+  }
 }
