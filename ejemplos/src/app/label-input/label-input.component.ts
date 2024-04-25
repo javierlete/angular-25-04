f8e61481ec9f002ae425fddb06bcad45be24b357
javier <javier@ipartekaula.com>
2024-04-25 16:55:38 +0200

    ng g c label-input --inline-template --inline-style --skip-tests

---
+++ ejemplos/src/app/label-input/label-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-label-input',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      label-input works!
+    </p>
+  `,
+  styles: ``
+})
+export class LabelInputComponent {
+
+}
