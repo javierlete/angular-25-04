1e62c5750eef09aeda1d31724bb0e4b86d599cb5
javier <javier@ipartekaula.com>
2024-04-30 18:13:03 +0200

    crud: registramos los componentes en app

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TablaComponent } from './tabla/tabla.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TablaComponent, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
