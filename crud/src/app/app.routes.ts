fade56422742834aa936d002c64809d38de06f75
javier <javier@ipartekaula.com>
2024-05-02 16:49:15 +0200

    crud: cambiamos las rutas de nuestros componentes

--- crud/src/app/app.routes.ts
+++ crud/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TablaComponent } from './tabla/tabla.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TablaComponent },
+    { path: 'form', component: FormularioComponent },
+    { path: 'form/:id', component: FormularioComponent },
+];
