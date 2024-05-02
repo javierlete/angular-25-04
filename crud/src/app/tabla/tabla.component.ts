40b0d92a7080f34ac094efbcfef81e02e9c9ce11
javier <javier@ipartekaula.com>
2024-05-02 16:59:37 +0200

    crud: tabla con datos de producto dentro de TypeScript

--- crud/src/app/tabla/tabla.component.ts
+++ crud/src/app/tabla/tabla.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Producto } from '../producto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabla',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './tabla.component.html',
   styleUrl: './tabla.component.css'
 })
 export class TablaComponent {
-
+  productos: Producto[] = [
+    { id: 1, nombre: 'Portátil', precio: 1234.12 },
+    { id: 2, nombre: 'Monitor', precio: 123.12 },
+    { id: 3, nombre: 'Ratón', precio: 12.12 },
+  ];
 }
