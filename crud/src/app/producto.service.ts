26b552ef8a0ba6618ed63f4d6f10186385e93608
javier <javier@ipartekaula.com>
2024-05-02 17:29:10 +0200

    crud: adaptación del servicio para json-server

--- crud/src/app/producto.service.ts
+++ crud/src/app/producto.service.ts
@@ -5,13 +5,10 @@ import { Producto } from './producto';
   providedIn: 'root'
 })
 export class ProductoService {
-  productos: Producto[] = [
-    { id: 1, nombre: 'Portátil', precio: 1234.12 },
-    { id: 2, nombre: 'Monitor', precio: 123.12 },
-    { id: 3, nombre: 'Ratón', precio: 12.12 },
-  ];
+  url = 'http://127.0.0.1:3000/productos/';
 
-  obtenerProductos(): Promise<Producto[]> {
-    return new Promise(resolve => resolve(this.productos));
+  async obtenerProductos(): Promise<Producto[]> {
+    const respuesta = await fetch(this.url);
+    return await respuesta.json();
   }
 }
