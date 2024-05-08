e0e8b626a61af2be567642508b77803aef68b50b
javier <javier@ipartekaula.com>
2024-05-08 18:26:31 +0200

    metube: valoración con estrellas

--- metube/src/app/componentes/valoracion/valoracion.component.ts
+++ metube/src/app/componentes/valoracion/valoracion.component.ts
@@ -1,17 +1,27 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-valoracion',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './valoracion.component.html',
   styleUrl: './valoracion.component.css'
 })
 export class ValoracionComponent {
+  @Input() tipo: 'cuadro' | 'estrellas' = 'estrellas';
   @Input() valoracion?: number;
   @Input() minimo = 0;
   @Input() maximo = 10;
   @Output() valoracionChange = new EventEmitter<number>();
+
+  obtenerArrayEstrellas() {
+    return Array(this.valoracion);
+  }
+  
+  obtenerArrayEstrellasHuecas() {
+    return Array(this.maximo - Number(this.valoracion));
+  }
 
   onCambio(valoracionEnElCuadro: number) {
     if(valoracionEnElCuadro >= this.minimo && valoracionEnElCuadro <= this.maximo) {
